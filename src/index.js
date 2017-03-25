411ff557f383801fae612fc05d64fc1e7cbb0bdf
Ibrahim Shabbir <muhammadibrahim1451@gmail.com>
2017-03-25 10:56:25 +0500

    Checked

--- src/index.js
+++ src/index.js
@@ -5,13 +5,7 @@ import './index.css';
 import firebase from 'firebase';
 
 // Initialize Firebase
- firebase.initializeApp ({
-    apiKey: "AIzaSyAzOHheguFXoCuXIPtx8UiheNw8GuXxRF0",
-    authDomain: "react-todo-app-2a191.firebaseapp.com",
-    databaseURL: "https://react-todo-app-2a191.firebaseio.com",
-    storageBucket: "react-todo-app-2a191.appspot.com",
-    messagingSenderId: "1038030051370"
-  });
+
   
 
 ReactDOM.render(
